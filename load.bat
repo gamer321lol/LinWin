81d139cd7478ee4e704e08ef79843c39c600deda
powdered <87124834+gamer321lol@users.noreply.github.com>
2021-08-24 08:15:55 +0100

    Update load.bat

--- load.bat
+++ load.bat
@@ -1,8 +1,9 @@
 @echo off
-// uh so this is all the code for loading lmao
+// loading stuff
 set mynonosquare=dont touch my nono sqaure
 cd bin
 title User@linwin-VM
 prompt User@Linwin-VM: 
 echo Welcome to LinWin (beta)
+cmd /k
 @echo on
